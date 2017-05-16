e576d321b80197ce319421680d68d94fb9000751
Willian Ribeiro Angelo <agfoccus@gmail.com>
2017-05-16 00:30:04 -0300

    curso

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import {Component, ViewChild} from "@angular/core";
+import {Nav, Platform} from "ionic-angular";
+import {StatusBar} from "@ionic-native/status-bar";
+import {SplashScreen} from "@ionic-native/splash-screen";
+
+import {HomePage} from "../pages/home/home";
+import {Deeplinks} from "@ionic-native/deeplinks";
+// import {ProdutoPage} from "../pages/produto/produto";
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  @ViewChild(Nav) navChild: Nav;
+  rootPage: any = HomePage;
+
+  constructor(platform: Platform,
+              statusBar: StatusBar,
+              deeplinks: Deeplinks,
+              splashScreen: SplashScreen) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+
+      // // Deeplink
+      // deeplinks.routeWithNavController(this.navChild, {
+      //   '/produto/:id': ProdutoPage,
+      // }).subscribe((match) => {
+      //   // match.$route - the route we matched, which is the matched entry from the arguments to route()
+      //   // match.$args - the args passed in the link
+      //   // match.$link - the full link data
+      //   console.log('Native route', match);
+      // }, (nomatch) => {
+      //   // nomatch.$link - the full link data
+      //   //console.log('Got a deeplink that didn\'t match', nomatch);
+      // });
+    });
+
+  }
+
+}
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import {BrowserModule} from "@angular/platform-browser";
+import {ErrorHandler, NgModule} from "@angular/core";
+import {IonicApp, IonicErrorHandler, IonicModule} from "ionic-angular";
+import {SplashScreen} from "@ionic-native/splash-screen";
+import {StatusBar} from "@ionic-native/status-bar";
+
+import {MyApp} from "./app.component";
+import {HomePage} from "../pages/home/home";
+import {ProdutoPageModule} from "../pages/produto/produto.module";
+import {ProdutoPage} from "../pages/produto/produto";
+import {Deeplinks} from "@ionic-native/deeplinks";
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    ProdutoPageModule,
+    IonicModule.forRoot(MyApp,
+      {
+        //locationStrategy: 'path',
+      },
+      {
+        // DeepLinker
+        links: [
+          {component: ProdutoPage, name: 'Produto', segment: 'produto/:codigo'},
+        ]
+      })
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    Deeplinks,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {
+}
